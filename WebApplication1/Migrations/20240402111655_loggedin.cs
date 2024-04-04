b058c44877acfe69d60e8e6ad423c5032758361d
Badr Ghazwani <154668314+bGhazwani@users.noreply.github.com>
2024-04-04 14:34:19 +0300

    migrations.

---
+++ WebApplication1/Migrations/20240402111655_loggedin.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class loggedin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "loggedin",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "loggedin",
+                table: "Users");
+        }
+    }
+}
